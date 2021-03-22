3719fc3e00246e8562a98c71eed16d7c155577b2
Kzis <driftkao@gmail.com>
2021-03-22 22:33:29 +0700

    add project b

---
+++ camel-microservices-b/src/test/java/com/demo/microservices/camelmicroservicesb/CamelMicroservicesBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.microservices.camelmicroservicesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelMicroservicesBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
